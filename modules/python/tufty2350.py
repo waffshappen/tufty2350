5cd2aa9926cc66a745e6589e1c9db4286489c881
thirdr <ryan@pimoroni.com>
2025-05-28 10:27:51 +0100

    Tufty2350: removed unused code

--- modules/python/tufty2350.py
+++ modules/python/tufty2350.py
@@ -1,10 +1,6 @@
 import machine
-import micropython
 from picographics import PicoGraphics, DISPLAY_EXPLORER
-import time
-import pcf85063a
 import cppmem
-
 
 BUTTON_DOWN = 11
 BUTTON_A = 12
@@ -13,26 +9,18 @@ BUTTON_C = 14
 BUTTON_UP = 15
 BUTTON_USER = 22
 
-BUTTON_MASK = 0b11111 << 11
-
 SYSTEM_VERY_SLOW = 0
 SYSTEM_SLOW = 1
 SYSTEM_NORMAL = 2
 SYSTEM_FAST = 3
 SYSTEM_TURBO = 4
 
-UPDATE_NORMAL = 0
-UPDATE_MEDIUM = 1
-UPDATE_FAST = 2
-UPDATE_TURBO = 3
-
-RTC_ALARM = 6
 LED = 8
-ENABLE_3V3 = 10
-BUSY = 9
+SENSOR_POWER = 9
+LIGHT_SENSE = 43
 
-WIDTH = 264
-HEIGHT = 176
+WIDTH = 320
+HEIGHT = 240
 
 SYSTEM_FREQS = [
     4000000,
@@ -50,15 +38,6 @@ BUTTONS = {
     BUTTON_UP: machine.Pin(BUTTON_UP, machine.Pin.IN, machine.Pin.PULL_DOWN),
 }
 
-
-i2c = machine.I2C(0)
-rtc = pcf85063a.PCF85063A(i2c)
-i2c.writeto_mem(0x51, 0x00, b"\x00")  # ensure rtc is running (this should be default?)
-rtc.enable_timer_interrupt(False)
-
-enable = machine.Pin(ENABLE_3V3, machine.Pin.OUT)
-enable.on()
-
 cppmem.set_mode(cppmem.MICROPYTHON)
 
 
@@ -73,94 +52,19 @@ def system_speed(speed):
         pass
 
 
-def turn_on():
-    enable.on()
-
-
-def turn_off():
-    time.sleep(0.05)
-    enable.off()
-    # Simulate an idle state on USB power by blocking
-    # until an RTC alarm or button event
-    rtc_alarm = machine.Pin(RTC_ALARM)
-    while True:
-        if rtc_alarm.value():
-            return
-        for button in BUTTONS.values():
-            if button.value():
-                return
-
-
-def pico_rtc_to_pcf():
-    # Set the PCF85063A to the time stored by Pico W's RTC
-    year, month, day, dow, hour, minute, second, _ = machine.RTC().datetime()
-    rtc.datetime((year, month, day, hour, minute, second, dow))
-
-
-def pcf_to_pico_rtc():
-    # Set Pico W's RTC to the time stored by the PCF85063A
-    t = rtc.datetime()
-    # BUG ERRNO 22, EINVAL, when date read from RTC is invalid for the Pico's RTC.
-    try:
-        machine.RTC().datetime((t[0], t[1], t[2], t[6], t[3], t[4], t[5], 0))
-        return True
-    except OSError:
-        return False
-
-
-def sleep_for(minutes):
-    year, month, day, hour, minute, second, dow = rtc.datetime()
-
-    # if the time is very close to the end of the minute, advance to the next minute
-    # this aims to fix the edge case where the board goes to sleep right as the RTC triggers, thus never waking up
-    if second >= 55:
-        minute += 1
-
-    # Can't sleep beyond a month, so clamp the sleep to a 28 day maximum
-    minutes = min(minutes, 40320)
-
-    # Calculate the future alarm date; first, turn the current time into seconds since epoch
-    sec_since_epoch = time.mktime((year, month, day, hour, minute, second, dow, 0))
-
-    # Add the required minutes to this
-    sec_since_epoch += minutes * 60
-
-    # And convert it back into a more useful tuple
-    (ayear, amonth, aday, ahour, aminute, asecond, adow, adoy) = time.localtime(sec_since_epoch)
-
-    # And now set the alarm as before, now including the day
-    rtc.clear_alarm_flag()
-    rtc.set_alarm(0, aminute, ahour, aday)
-    rtc.enable_alarm_interrupt(True)
-
-    turn_off()
-
-
 class Tufty2350():
     def __init__(self):
-        self.display = PicoGraphics(DISPLAY_EXPLORER, roate=180)
+        self.display = PicoGraphics(DISPLAY_EXPLORER, rotate=180)
         self._led = machine.PWM(machine.Pin(LED))
         self._led.freq(1000)
         self._led.duty_u16(0)
-        self._update_speed = 0
 
     def __getattr__(self, item):
         # Glue to redirect calls to PicoGraphics
         return getattr(self.display, item)
 
     def update(self):
-        t_start = time.ticks_ms()
         self.display.update()
-        t_elapsed = time.ticks_ms() - t_start
-
-        delay_ms = [4700, 2600, 900, 250][self._update_speed]
-
-        if t_elapsed < delay_ms:
-            time.sleep((delay_ms - t_elapsed) / 1000)
-
-    def set_update_speed(self, speed):
-        self.display.set_update_speed(speed)
-        self._update_speed = speed
 
     def led(self, brightness):
         brightness = max(0, min(255, brightness))
@@ -172,12 +76,6 @@ class Tufty2350():
     def thickness(self, _thickness):
         raise RuntimeError("Thickness not supported in PicoGraphics.")
 
-    def halt(self):
-        turn_off()
-
-    def keepalive(self):
-        turn_on()
-
     def pressed(self, button):
         return BUTTONS[button].value() == 1
 
@@ -186,64 +84,3 @@ class Tufty2350():
             if button.value():
                 return True
         return False
-
-    @micropython.native
-    def icon(self, data, index, data_w, icon_size, x, y):
-        s_x = (index * icon_size) % data_w
-        s_y = int((index * icon_size) / data_w)
-
-        for o_y in range(icon_size):
-            for o_x in range(icon_size):
-                o = ((o_y + s_y) * data_w) + (o_x + s_x)
-                bm = 0b10000000 >> (o & 0b111)
-                if data[o >> 3] & bm:
-                    self.display.pixel(x + o_x, y + o_y)
-
-    def image(self, data, w, h, x, y):
-        for oy in range(h):
-            row = data[oy]
-            for ox in range(w):
-                if row & 0b1 == 0:
-                    self.display.pixel(x + ox, y + oy)
-                row >>= 1
-
-    def status_handler(self, mode, status, ip):
-        self.display.set_update_speed(2)
-        print(mode, status, ip)
-        self.display.set_pen(15)
-        self.display.clear()
-        self.display.set_pen(0)
-        if status:
-            self.display.text("Connected!", 10, 10, 300, 0.5)
-            self.display.text(ip, 10, 30, 300, 0.5)
-        elif status is None:
-            self.display.text("Connecting...", 10, 10, 300, 0.5)
-        else:
-            self.display.text("Connection failed!", 10, 10, 300, 0.5)
-        self.update()
-
-    def isconnected(self):
-        import network
-        return network.WLAN(network.STA_IF).isconnected()
-
-    def ip_address(self):
-        import network
-        return network.WLAN(network.STA_IF).ifconfig()[0]
-
-    def connect(self, **args):
-        from network_manager import NetworkManager
-        import WIFI_CONFIG
-        import uasyncio
-        import gc
-
-        status_handler = args.get("status_handler", self.status_handler)
-        error_handler = args.get("error_handler", None)
-        client_timeout = args.get("timeout", 60)
-        retries = args.get("retries", 2)
-
-        if WIFI_CONFIG.COUNTRY == "":
-            raise RuntimeError("You must populate WIFI_CONFIG.py for networking.")
-
-        network_manager = NetworkManager(WIFI_CONFIG.COUNTRY, client_timeout=client_timeout, status_handler=status_handler, error_handler=error_handler, retries=retries)
-        uasyncio.get_event_loop().run_until_complete(network_manager.client(WIFI_CONFIG.SSID, WIFI_CONFIG.PSK))
-        gc.collect()
