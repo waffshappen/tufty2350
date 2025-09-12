a8986dc69dde3c10ad47b4539e07f7c9eceb516a
ZodiusInfuser <christopher.parrott2@gmail.com>
2025-09-12 16:25:36 +0100

    Temp: Add back in icon and image functions. Remove HOME from buttons list

--- modules/python/tufty2350.py
+++ modules/python/tufty2350.py
@@ -34,8 +34,7 @@ BUTTONS = {
     BUTTON_A,
     BUTTON_B,
     BUTTON_C,
-    BUTTON_UP,
-    BUTTON_HOME
+    BUTTON_UP
 }
 
 LIGHT_SENSOR = machine.ADC(machine.Pin("LIGHT_SENSE"))
@@ -89,6 +88,26 @@ class Tufty2350():
 
     def pressed_any(self):
         return 0 in [button.value() for button in BUTTONS]
+
+    @micropython.native
+    def icon(self, data, index, data_w, icon_size, x, y):
+        s_x = (index * icon_size) % data_w
+        s_y = int((index * icon_size) / data_w)
+
+        for o_y in range(icon_size):
+            for o_x in range(icon_size):
+                o = ((o_y + s_y) * data_w) + (o_x + s_x)
+                bm = 0b10000000 >> (o & 0b111)
+                if data[o >> 3] & bm:
+                    self.display.pixel(x + o_x, y + o_y)
+
+    def image(self, data, w, h, x, y):
+        for oy in range(h):
+            row = data[oy]
+            for ox in range(w):
+                if row & 0b1 == 0:
+                    self.display.pixel(x + ox, y + oy)
+                row >>= 1
 
     def sleep(self):
         self.display.set_backlight(0)
