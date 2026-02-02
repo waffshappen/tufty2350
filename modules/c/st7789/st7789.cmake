3867768085c02a3d46edbf802826d0e4c00d80ee
waffshappen <44290023+waffshappen@users.noreply.github.com>
2026-02-02 21:00:44 +0100

    Only include once

--- modules/c/st7789/st7789.cmake
+++ modules/c/st7789/st7789.cmake
@@ -8,7 +8,5 @@ pico_generate_pio_header(${DRIVER_NAME} ${CMAKE_CURRENT_LIST_DIR}/st7789_paralle
 
 target_include_directories(${DRIVER_NAME} INTERFACE ${CMAKE_CURRENT_LIST_DIR})
 
-target_include_directories(st7789 INTERFACE ${CMAKE_CURRENT_LIST_DIR})
-
 # Pull in pico libraries that we need
 target_link_libraries(${DRIVER_NAME} INTERFACE pico_stdlib hardware_pwm hardware_pio hardware_dma)
